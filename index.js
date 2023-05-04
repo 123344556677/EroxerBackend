2c191b5b779ddde81b214d822a472f29c18aa025
AbdulMannan370 <58433372+AbdulMannan370@users.noreply.github.com>
2023-05-04 08:26:14 +0500

    Second commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(bodyParser.json({ extended: true,limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-app.listen(5000);
+app.listen(PORT);
 app.use('/', router)
 connection(URL);
 
