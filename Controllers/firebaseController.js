d31638ed37de2aa3dd2776287df292bc1f1d69a4
AbdulMannan370 <58433372+AbdulMannan370@users.noreply.github.com>
2023-09-23 23:02:27 +0500

    second commit

--- Controllers/firebaseController.js
+++ Controllers/firebaseController.js
@@ -1,41 +1,30 @@
 import firebaseToken from "../Schemas/FirebaseTokens.js";
-import {initializeApp, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import registeringUser from "../Schemas/Auth.js";
+import admin from "firebase-admin";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
+// import serviceAccount from "file:///D:/react/react%20course/eroxr/backend/contact-data-6e109-firebase-adminsdk-dx9uw-179b5365ff.json" assert { type: "json" };
 
 initializeApp({
   credential: applicationDefault(),
-  projectId: 'potion-for-creators',
+  projectId: "erroxrapp",
 });
 
-
 export const Notification = async (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
   const { title, body, callerId, recieverId, type } = req.body;
 
   if (!callerId) {
-    return res.json({ status: 500, message: 'Caller ID is required' });
+    return res.json({ status: 500, message: "Caller ID is required" });
   }
 
   // Define notification payload
-  const payload = {
-    notification: {
-      title,
-      body,
-    },
-    data: {
-      callerId,
-      recieverId,
-      type,
-    },
-  };
 
   // Define FCM options with high priority
   const options = {
-    priority: 'high',
+    priority: "high",
   };
 
   try {
@@ -43,31 +32,34 @@ export const Notification = async (req, res) => {
     const userToken = await firebaseToken.findOne({ userId: recieverId });
 
     if (!userToken) {
-      return res.json({ status: 500, message: 'User token not found' });
+      return res.json({ status: 500, message: "User token not found" });
     }
-   
 
     // Fetch caller data (assuming you have a schema/model named registeringUser)
     const callerData = await registeringUser.findOne({ _id: callerId });
 
     if (!callerData) {
-      return res.json({ status: 500, message: 'Caller data not found' });
+      return res.json({ status: 500, message: "Caller data not found" });
     }
 
-    const data = {
-      callerId: callerId,
-      recieverId: recieverId,
-      callerData: callerData,
-      type: type,
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      data: {
+        callerId: callerId,
+        recieverId: recieverId,
+        type: type,
+      },
+      token: userToken.token, // Set the user's FCM token here
     };
 
-    payload.data.callerData = callerData;
-
     // Send the notification to the user's token
-    getMessaging().sendToDevice(userToken.token, payload, options);
+    getMessaging().send(message);
 
-    res.json({ status: 200, message: 'Notification sent successfully', data: data });
+    res.json({ status: 200, message: "Notification sent successfully", data:message});
   } catch (error) {
-    res.json({ status: 500, message: 'Error: ' + error });
+    res.json({ status: 500, message: "Error: " + error });
   }
 };
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import connection from './database/db.js'
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from './routes/routes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const PORT = process.env.PORT || 5000;
 // import { Server } from 'socket.io';
 // import { createServer } from 'http';
